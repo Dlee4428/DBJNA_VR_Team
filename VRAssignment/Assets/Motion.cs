a9852e3f7c19d3fc864eca36d09f04ee72c2a563
Dlee4428 <44447609+Dlee4428@users.noreply.github.com>
2019-11-06 17:20:16 -0500

    Upload Brandon's base

---
+++ VRAssignment/Assets/Motion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+    float horizontal, vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal -= 2 * Input.GetAxis("Mouse Y");
+        vertical += 2 * Input.GetAxis("Mouse X");
+        transform.eulerAngles = new Vector3(horizontal, vertical, 0);
+    }
+}
