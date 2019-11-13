afeb468785feba0e5602217315229b0398b68ff7
Jaymie Xu <jake9828@gmail.com>
2019-11-12 20:22:06 -0500

    Added some sample POI

---
+++ VRAssignment/Assets/Scripts/POI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class POI : Stareable
+{
+    [SerializeField]
+    private GameObject target;
+
+    public override void BeginStare()
+    {
+        target.SetActive(true);
+    }
+
+    public override void EndStare()
+    {
+        target.SetActive(false);
+    }
+}
